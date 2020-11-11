4f7d8413f7b493b5008828a26ab0a6d88de847d9
nikan_af1 <nikan_af1@yahoo.com>
2020-11-10 23:16:02 -0500

    added the footer and the main page.

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-component',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+    onClick() {
+        console.log('here');
+    }
+}
