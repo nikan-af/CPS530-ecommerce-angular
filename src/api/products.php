19172275a661bfe0c475b89286d037040a465584
nikan-af <>
2020-11-15 22:19:03 -0500

    The api  end point for fetching products from the database.

---
+++ src/api/products.php
@@ -0,0 +1,24 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+    include_once("database.php");
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+
+    if(isset($postdata) && !empty($postdata)) {
+        $gender = mysqli_real_escape_string($mysqli, trim($request->gender));
+        $sql = '';
+        $sql = "SELECT * FROM cps530.products where gender='$gender'";
+        if($result = mysqli_query($mysqli,$sql)) {
+            $rows = array();
+            while($row = mysqli_fetch_assoc($result)) {
+                $rows[] = $row;
+            }
+            echo json_encode($rows);
+        } else {
+            http_response_code(404);
+        }
+    }
+?>
